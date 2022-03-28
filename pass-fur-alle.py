7969f2c367b4a8fdabd40641a0a7e0c83472f64a
Johan Jonk Stenström <jonk@pirateboy.net>
2022-03-28 21:18:31 +0200

    Remove unused import

--- pass-fur-alle.py
+++ pass-fur-alle.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select    
 from datetime import datetime
