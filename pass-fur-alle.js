65eceb177c62f34ce08ddc1451bd97c00ae30791
NoNameForMee <NoNameForMee@users.noreply.github.com>
2022-04-10 09:45:41 +0200

    Directly get date "today" as ISO 8601 string (#78)
    Avoid computing intermediate variables for year, month and day (possibly 0-padded) just to construct an ISO 8601 string of "YYYY-MM-DD".

--- pass-fur-alle.js
+++ pass-fur-alle.js
@@ -125,10 +125,7 @@
 
     function today() {
         var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0');
-        var yyyy = today.getFullYear();
-        return yyyy + '-' + mm + '-' + dd;
+        return today.toISOString().slice(0,10);
     }
 
     function setButtonTexts() {
